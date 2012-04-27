a81c47dee2d73f96a0fc33324484adce8ba2c893
Junyi <michaelf614@gmail.com>
2012-04-27 03:49:10 -0700

    create game app

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,109 @@
+class PagesController < ApplicationController
+  before_filter :common_content
+
+  def common_content
+	@defeat={rock: :scissors, paper: :rock, scissors: :paper, lizard: :paper, spock: :rock}
+	@defeat2 = {rock: :lizard, paper: :spock, scissors: :lizard, lizard: :spock, spock: :scissors}
+	@throws=@defeat.keys
+  end
+  
+  def home
+    session[:t]=0
+	session[:l]=0
+	session[:w]=0
+  end
+  
+  def result
+    @computer_throw=@throws.sample 
+	@player_throw= params[:type].to_sym
+    if @computer_throw == @player_throw 
+	  @title="You tied with the computer. Try again!" 
+	  session[:t] ||= 0
+	  session[:t] +=1
+	elsif (@computer_throw == @defeat[@player_throw] or @computer_throw == @defeat2[@player_throw] )
+      @title="Nicely done; #{@player_throw} beats #{@computer_throw}!" 
+	  session[:w] ||= 0
+	  session[:w] +=1
+    else   
+      @title="Ouch; #{@computer_throw} beats #{@player_throw}. Better luck next time!" 
+      session[:l] ||= 0
+	  session[:l] +=1
+    end
+  end
+    def paper
+    @computer_throw=@throws.sample
+	@player_throw="paper".to_sym
+    if @computer_throw == @player_throw
+	  @title="You tied with the computer. Try again!" 
+      session[:t] ||= 0	
+	  session[:t] +=1
+	elsif (@computer_throw == @defeat[@player_throw] or @computer_throw == @defeat[@player_throw]) 
+      @title="Nicely done; #{@player_throw} beats #{@computer_throw}!"  
+	  session[:w] ||= 0
+	  session[:w] +=1
+    else 
+      @title="Ouch; #{@computer_throw} beats #{@player_throw}. Better luck next time!" 
+      session[:l] ||= 0
+	  session[:l] +=1
+    end
+  end
+  def scissors
+    @computer_throw=@throws.sample
+	@player_throw="scissors".to_sym
+    if @computer_throw == @player_throw
+	  @title="You tied with the computer. Try again!" 
+	  session[:t] ||= 0
+	  session[:t] +=1
+	elsif (@computer_throw == @defeat[@player_throw] or @computer_throw == @defeat[@player_throw] )
+      @title="Nicely done; #{@player_throw} beats #{@computer_throw}!" 
+	  session[:w] ||= 0
+	  session[:w] +=1
+    else 
+      @title="Ouch; #{@computer_throw} beats #{@player_throw}. Better luck next time!" 
+      session[:l] ||= 0
+	  session[:l] +=1
+    end
+  end
+def lizard
+    @computer_throw=@throws.sample
+	@player_throw="lizard".to_sym
+    if @computer_throw == @player_throw
+	@title="You tied with the computer. Try again!"  
+	session[:t] ||= 0
+	session[:t] +=1
+	elsif (@computer_throw == @defeat[@player_throw] or @computer_throw == @defeat[@player_throw] )
+    @title="Nicely done; #{@player_throw} beats #{@computer_throw}!" 
+	session[:w] ||= 0
+	session[:w] +=1
+    else 
+    @title="Ouch; #{@computer_throw} beats #{@player_throw}. Better luck next time!" 
+    session[:l] ||= 0
+	session[:l] +=1
+    end
+  end
+def spock
+    @computer_throw=@throws.sample
+	@player_throw="spock".to_sym
+    if @computer_throw == @player_throw
+	@title="You tied with the computer. Try again!" 
+	session[:t] ||= 0
+	session[:t] +=1
+	elsif (@computer_throw == @defeat[@player_throw] or @computer_throw == @defeat[@player_throw] )
+    @title="Nicely done; #{@player_throw} beats #{@computer_throw}!" 
+	session[:w] ||= 0
+	session[:w] +=1
+    else 
+    @title="Ouch; #{@computer_throw} beats #{@player_throw}. Better luck next time!" 
+    session[:l] ||= 0
+	session[:l] +=1
+    end
+  end
+  
+
+  def stat
+    @t=session[:t]
+	@w=session[:w]
+	@l=session[:l]
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gameapp::Application.config.secret_token = 'f5eeba9f75b3f51f45924823ce2bd117220f984791c35ea0878fc70fed501f1c1ea0e1ed550f4c8017aec7a06bb74b593227f36b5f40e44fa3891b62751fac1a'
